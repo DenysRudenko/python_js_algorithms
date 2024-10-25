b64e3a33143d45aa54d4f38e0445104f28edb247
Denys Rudenko <69275031+DenysRudenko@users.noreply.github.com>
2024-10-25 23:32:32 +0100

    update(challenge4): solved summer_69 puzzle

--- python/challenges/challenge_four.py
+++ python/challenges/challenge_four.py
@@ -170,3 +170,33 @@ def black_jack(a, b, c):
 print(black_jack(5, 6, 7))
 print(black_jack(9, 9, 9))
 print(black_jack(9, 9, 11))
+print('-' * 20)
+
+
+# SUMMER OF '69: Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and extending to the next 9 
+# (every 6 will be followed by at least one 9). Return 0 for no numbers.
+# summer_69([1, 3, 5]) --> 9
+# summer_69([4, 5, 6, 7, 8, 9]) --> 9
+# summer_69([2, 1, 6, 9, 11]) --> 14
+
+def summer_69(arr):
+    total = 0
+    ignore = False
+
+    for num in arr:
+        if num == 6:
+            ignore = True
+        elif num == 9 and ignore:
+            ignore = False
+        elif not ignore:
+            total += num
+
+    return total
+
+
+ 
+
+print(summer_69([]))
+print(summer_69([1, 3, 5]))
+print(summer_69([4, 5, 6, 7, 8, 9]))
+print(summer_69([2, 1, 6, 9, 11]))
