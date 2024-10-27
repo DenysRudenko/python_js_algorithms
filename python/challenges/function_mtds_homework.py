bd446986769a378e4eb3cd92b9f7d3752b8f06f7
Denys Rudenko <69275031+DenysRudenko@users.noreply.github.com>
2024-10-27 15:48:45 +0000

    update(function_homework): solved polindrome puzzle

--- python/challenges/function_mtds_homework.py
+++ python/challenges/function_mtds_homework.py
@@ -103,3 +103,30 @@ def multiply(numbers):
     return result
 
 print(multiply([1,2,3,-4]))
+print('-' * 21)
+
+
+
+# Write a Python function that checks whether a word or phrase is palindrome or not.
+
+# Note: A palindrome is word, phrase, or sequence that reads the same backward as forward,
+# e.g., madam,kayak,racecar, or a phrase "nurses run".
+# Hint: You may want to check out the .replace() method in a string to help out with dealing with spaces.
+# Also google search how to reverse a string in Python, there are some clever ways to do it with slicing notation.
+
+
+def palindrome(s):
+    my_string = ''.join(s.split())
+
+    return my_string == my_string[::-1]
+    
+
+print(palindrome("helleh"))
+print(palindrome("madam"))
+print(palindrome("kayak"))
+print(palindrome("racecar"))
+print(palindrome("nurses run"))
+print(palindrome("hello"))
+print(palindrome("cat"))
+
+
