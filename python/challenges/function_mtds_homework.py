6ede6b03142e4ce586546b319dbd011082b3fced
Denys Rudenko <69275031+DenysRudenko@users.noreply.github.com>
2024-10-27 14:27:12 +0000

    update(function_homework): solved run_check and run_bool puzzle

--- python/challenges/function_mtds_homework.py
+++ python/challenges/function_mtds_homework.py
@@ -13,3 +13,35 @@ def vol(rad):
 
 
 print(vol(2))
+print('-'* 21)
+
+# Write a function that checks whether
+# a number is in a given range (inclusive of high and low)
+
+def ran_check(num, low, high):
+
+    if num in range(low, high + 1):
+        return f'{num} is in the range between {low} and {high}'
+    else:
+        return f'{num} is out of bounds!'
+
+
+print(ran_check(8, 2, 7))
+print(ran_check(5, 2, 7))
+print('-' * 21)
+
+
+def run_bool(num, low, high):
+    
+    if num in range(low, high + 1):
+        return True
+    else:
+        return False
+
+
+print(run_bool(3, 1, 10))
+print(run_bool(11, 1, 10))
+print('-' * 21)
+
+
+
