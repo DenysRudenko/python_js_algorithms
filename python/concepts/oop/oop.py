5788098aeeeea87191dab5e8461c8ed26f34920e
Denys Rudenko <69275031+DenysRudenko@users.noreply.github.com>
2024-11-12 16:53:04 +0000

    update(oop): created a basic triangle class that will calculate the area

--- python/concepts/oop/oop.py
+++ python/concepts/oop/oop.py
@@ -7,8 +7,10 @@ print(my_set)
 class Dog():
     
     # Сlass object attribute
+    # same for any instance of a class
+    species = "mammal"
     
-    def __init__(self, breed, name, spots):
+    def __init__(self, breed, name):
 
         # Attributes
         # We take in the argument
@@ -16,9 +18,53 @@ class Dog():
         self.breed = breed
         self.name = name
 
-        # Expect boolean True/False
-        self.spots = spots
+    # OPERATIONS/Actions ---> Methods
+    def bark(self, number):
+        print("Woof! My name is {} and the number is {}!".format(self.name, number))
 
-my_dog = Dog(breed='Labrador', name='Marley', spots=False)
+my_dog = Dog("Labrador", "Frankie")
 
-print(my_dog.name)
+my_dog.bark(10)
+
+print(my_dog.species)
+
+
+class Circle():
+    
+    # Class object attribute
+    pi = 3.14
+
+    def __init__(self, radius=1):
+
+        self.radius = radius
+        self.area = radius * radius * self.pi
+
+    # method 
+    def get_circumference(self):
+        return self.radius * self.pi * 2
+
+
+my_circle = Circle(30)
+
+print(my_circle.pi)
+
+print(my_circle.area)
+
+print(my_circle.get_circumference())
+print("-" * 30)
+
+
+class Triangle():
+
+    def __init__(self, base=3, height=4):
+        
+        self.base = base
+        self.height = height
+
+    def get_area(self):
+        return 1/2 * (self.base * self.height)
+    
+
+triangle_area = Triangle()
+
+print(triangle_area.get_area())
